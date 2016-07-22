c4aa68801e06381b6f9abf6c7ba23b9e5733b781
Peter Rudenko <peter.rudenko@gmail.com>
2016-07-22 20:14:50 +0400

    Updated the copyright headers.

--- Source/TrainingContext.h
+++ Source/TrainingContext.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2015 Peter Rudenko
+    Copyright (c) 2016 Peter Rudenko
 
     Permission is hereby granted, free of charge, to any person obtaining
     a copy of this software and associated documentation files (the "Software"),
